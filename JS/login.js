cced3f15b2a8669973caa27d2e2d9d5a2e79b873
Vishwanath K Khuli <152583197+vishwanathkhuli@users.noreply.github.com>
2024-08-16 17:32:51 +0530

    first commit

---
+++ JS/login.js
@@ -0,0 +1,9 @@
+function showSignIn() {
+    document.getElementById('signIn-container').style.display = 'flex';
+    document.getElementById('signUp-container').style.display = 'none';
+}
+
+function showSignUp() {
+    document.getElementById('signIn-container').style.display = 'none';
+    document.getElementById('signUp-container').style.display = 'flex';
+}
